402e997f7067c5164d9142677ae16b5d8fe89aad
TobiasKlein2004 <kleintobias2004@gmail.com>
2023-08-17 18:57:26 +0200

    Angle to Point conversion completed

--- main.py
+++ main.py
@@ -1,14 +1,16 @@
 import pygame
 import random
+import math
 
 pygame.init()
 
 # Constant Variables
 WINDOW_WIDTH = 700
 WINDOW_HEIGHT = 700
-FPS = 30
+FPS = 10
 PLAYER_SIZE = 20
 PLAYER_SPEED = 10
+PLAYER_START_DIRECTION = 45
 
 # colors
 BACKGROUND_COLOR = (0,0,0)
@@ -28,77 +30,22 @@ class Player():
         self.speed = speed
     
     def draw(self):
-        match self.direction:
-            case 0:
-                self.y -= 1*self.speed
-            case 45:
-                self.x += 1*self.speed
-                self.y -= 1*self.speed
-            case 90:
-                self.x += 1*self.speed
-            case 135:
-                self.x += 1*self.speed
-                self.y += 1*self.speed
-            case 180:
-                self.y += 1*self.speed
-            case 225:
-                self.y += 1*self.speed
-                self.x -= 1*self.speed
-            case 270:
-                self.x -= 1*self.speed
-            case 315:
-                self.x -= 1*self.speed
-                self.y -= 1*self.speed
-
-        pygame.draw.rect(screen, PLAYER_COLOR, pygame.Rect(self.x, self.y, self.size, self.size))
+        targetX, targetY = self.angleToCoordinats(self.direction)
+        targetX = round(targetX * 700)
+        targetY = round(targetY * 700)
+        pygame.draw.rect(screen, PLAYER_COLOR, pygame.Rect(self.x - (self.size / 2), self.y - (self.size / 2), self.size, self.size))
+        pygame.draw.rect(screen, (0, 255, 0), pygame.Rect(self.x, self.y, 1, 1))
+        pygame.draw.line(screen, (255, 255, 255), (self.x, self.y), (self.x + targetX, self.y + targetY))
     
-    def changeDirection(self, direction, wallDir):
-        acceptedDirections = [0, 45, 90, 135, 180, 225, 270, 315]
-        standingOpposits = [(45, 315), (135, 225), (90, 270)]
-        layingOpposits = [(45, 135), (315, 225), (0, 180)]
+    def angleToCoordinats(self, angle):
+        radiants = -1*((angle - 180) * (math.pi / 180))   # make 0deg uplooking and going clockwise
+        x = math.sin(radiants)
+        y = math.cos(radiants)
+        #print("X: " + str(x) + " - Y: " + str(y))
+        return (x, y)
 
-        if direction not in acceptedDirections:
-            print("(Error) Unaccepted direction value: " + str(direction))
-            raise Exception
-        
-        print("Current Dir.: " + str(direction))
-
-        tempDir = 0
-        if wallDir == 0:    #aka standing
-            print("STANDING - - - - - - - - - - -")
-            for pair in standingOpposits:
-                if pair[0] == direction:
-                    tempDir = pair[1]
-                elif pair[1] == direction:
-                    tempDir = pair[0]
-        elif wallDir == 1:    #aka laying
-            print("LAYING - - - - - - - - - - -")
-            for pair in layingOpposits:
-                if pair[0] == direction:
-                    tempDir = pair[1]
-                elif pair[1] == direction:
-                    tempDir = pair[0]
-        
-        self.direction = tempDir
-    
-    def formatDirection(self, direction):
-        if self.direction + direction < 0:
-            return -1*(self.direction + direction)
-        elif self.direction + direction > 360:
-            print(self.direction + direction - 360)
-            return (self.direction + direction) - 360
-        elif self.direction + direction == 360:
-            return 0
-        else: 
-            return self.direction + direction
-    
-    def checkCollision(self, walls):
-        neighbors = [(self.x - 1, self.y), (self.x, self.y - 1), (self.x + 1, self.y), (self.x, self.y + 1)]
-        for neighbor in neighbors:
-            for wall in walls:
-                info = wall.info()
-                if neighbor in info[3]:    #aka neighbor in the area/pixels of wall
-                    self.changeDirection(self.direction, info[2])
+    def updateAngle(self, angle):
+        self.direction = angle
 
 class Wall():
     def __init__(self, size, pos, direction):
@@ -124,17 +71,18 @@ def drawWalls(walls):
     for wall in walls:
         wall.draw()
 
-player = Player(PLAYER_SIZE, (200, 500), 315, PLAYER_SPEED)
+player = Player(PLAYER_SIZE, (350, 350), PLAYER_START_DIRECTION, PLAYER_SPEED)
 
 # wall = wall((width, height), (xPos, yPos), orientation) <- 0 standing, 1 laying
-wall1 = Wall((100, 700), (0, 0), 0)
-wall2 = Wall((100, 700), (600, 0), 0)
-wall3 = Wall((700, 100), (0, 0), 1)
-wall4 = Wall((700, 100), (0, 600), 1)
+wall1 = Wall((50, 700), (0, 0), 0)
+wall2 = Wall((50, 700), (650, 0), 0)
+wall3 = Wall((700, 50), (0, 0), 1)
+wall4 = Wall((700, 50), (0, 650), 1)
 Walls = [wall1, wall2, wall3, wall4]
 
 running = True
 
+counter = 0
 while running == True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -146,13 +94,14 @@ while running == True:
     drawWalls(Walls)
 
     # game Logic
-    player.checkCollision(Walls)
 
     # refresh
     pygame.display.update()
 
     # set fps
     pygame.time.Clock().tick(FPS)
+
+    counter += 1
 
 pygame.quit()
 
