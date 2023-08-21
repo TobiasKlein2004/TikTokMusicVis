5b0aa58177a1f18d0869b993f4d7f04608560404
TobiasKlein2004 <kleintobias2004@gmail.com>
2023-08-21 13:24:14 +0200

    static target in init; debug overlay

--- main.py
+++ main.py
@@ -5,13 +5,16 @@ import math
 pygame.init()
 
 # Constant Variables
+DEBUG = True
 WINDOW_WIDTH = 700
 WINDOW_HEIGHT = 700
 FPS = 10
 PLAYER_SIZE = 20
 PLAYER_SPEED = 2            #higher means faster
 PLAYER_STEPS = 200             #lower means faster
-PLAYER_START_DIRECTION = 300
+PLAYER_START_DIRECTION = 90
+PLAYER_START_POSITION = (0, 0)
+PATH_PROJECTION_LENGHT = 250
 
 # colors
 BACKGROUND_COLOR = (0,0,0)
@@ -23,6 +26,22 @@ pygame.display.set_caption("Musik Game")
 screen = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_WIDTH))
 
 
+# Debug - - - - - - - - - - - - - - - - - -
+def debugDraw(target, pos):
+    # draw grid
+    gridgapsize = PLAYER_SIZE
+    linecolor = (50, 50, 50)
+    for i in range(WINDOW_WIDTH):
+        if i % gridgapsize == 0:
+            pygame.draw.line(screen, linecolor, (i, 0), (i, WINDOW_HEIGHT))
+    for i in range(WINDOW_HEIGHT):
+        if i % gridgapsize == 0:
+            pygame.draw.line(screen, linecolor, (0, i), (WINDOW_WIDTH, i))
+    pygame.draw.circle(screen, (255, 165, 0), toPygameCoordinates(target), PLAYER_SIZE / 4)
+    pygame.draw.line(screen, (255, 255, 255), pos, toPygameCoordinates(target))
+# Debug End - - - - - - - - - - - - - - - -
+
+
 def toPygameCoordinates(coords):
     return (coords[0] + WINDOW_WIDTH / 2, coords[1] + WINDOW_HEIGHT / 2)
 
@@ -32,20 +51,21 @@ class Player():
         self.x, self.y = pos
         self.direction = direction
         self.speed = speed
+        self.targetX, self.targetY = round(self.angleToCoordinats(self.direction)[0] * PATH_PROJECTION_LENGHT), round(self.angleToCoordinats(self.direction)[1] * PATH_PROJECTION_LENGHT)
     
     def drawPlayerBox(self, pixels):
         for pixel in pixels:
             pygame.draw.rect(screen, (random.choice(range(0, 255)), random.choice(range(0, 255)), random.choice(range(0, 255))), pygame.Rect(pixel[0], pixel[1], 1, 1))
     
     def draw(self, walls):
-        targetX, targetY = self.angleToCoordinats(self.direction)
-        targetX = round(targetX * 700)
-        targetY = round(targetY * 700)
-        #print("X Target: " + str(targetX) + "  |  Y Target: " + str(targetY))
+        #targetX, targetY = self.angleToCoordinats(self.direction)
+        #self.targetX = round(self.targetX * 700)
+        #self.targetY = round(self.targetY * 700)
+        print("X Target: " + str(self.targetX) + "  |  Y Target: " + str(self.targetY))
 
-        targetX_Distance = targetX - self.x
-        targetY_Distance = targetY - self.y
-        #print("X Distance: " + str(targetX_Distance) + "  |  Y Distance: " + str(targetY_Distance))
+        targetX_Distance = self.targetX - self.x
+        targetY_Distance = self.targetY - self.y
+        print("X Distance: " + str(targetX_Distance) + "  |  Y Distance: " + str(targetY_Distance))
 
         self.x += (targetX_Distance / PLAYER_STEPS * PLAYER_SPEED)
         self.y += (targetY_Distance / PLAYER_STEPS * PLAYER_SPEED)
@@ -60,10 +80,12 @@ class Player():
                 playerPixels.append((j,i))
 
         # Actual drawing
+        if DEBUG:
+            debugDraw((self.targetX, self.targetY), (pgx, pgy))
         self.drawPlayerBox(playerPixels)
         for wall in walls:
             wall.draw()
-        pygame.draw.line(screen, (255, 255, 255), (pgx, pgy), (pgx + targetX, pgy + targetY))
+        #pygame.draw.line(screen, (255, 255, 255), (pgx, pgy), (pgx + self.targetX, pgy + self.targetY))
     
     def angleToCoordinats(self, angle):
         radiants = -1*((angle - 180) * (math.pi / 180))   # make 0deg uplooking and going clockwise
@@ -153,7 +175,7 @@ class Wall():
 
 
 
-player = Player(PLAYER_SIZE, (250, -250), PLAYER_START_DIRECTION, PLAYER_SPEED)
+player = Player(PLAYER_SIZE, PLAYER_START_POSITION, PLAYER_START_DIRECTION, PLAYER_SPEED)
 
 # wall = wall((width, height), (xPos, yPos), orientation) <- 0 standing, 1 laying
 wall1 = Wall((50, 700), (0, 0), 0)
